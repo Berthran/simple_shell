8fbd31a8a527d8fd09cf5fd4be599ff97c97eb49
Daniel Berthran <berthlearn@gmail.com>
2023-03-30 06:16:33 +0100

    betty fix

--- pid.c
+++ pid.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main()
+/**
+ * main - prints the parent id of a process
+ *
+ * Return: void
+ */
+
+int main(void)
 {
 	pid_t pidp;
+
 	pidp = getppid();
 	printf("%u\n", pidp);
 	return (0);
